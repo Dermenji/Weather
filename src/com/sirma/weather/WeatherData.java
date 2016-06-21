787dfa0159beee84ca82e0b752fbb70e67cc2644
Dermenji <Princess_Alice2016>
2016-06-21 18:11:52 +0300

    add to list

--- src/com/sirma/weather/WeatherData.java
+++ src/com/sirma/weather/WeatherData.java
@@ -3,4 +3,19 @@ package com.sirma.weather;
 
 public class WeatherData {
 
+    private ReadingDAO dao;
+
+    public void importData(String file){
+        Reading read = new Reading();
+        read.setDateTime();
+        read.setCity();
+        read.setMeasurement();
+        read.setReadingType();
+        read.setValues();
+
+        dao.addReading(read);
+    }
+
+
+
 }
